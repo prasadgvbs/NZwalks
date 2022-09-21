c937078f1d78b9b05e213c1d30b51b1701822cab
prasadgvbs <prasadgvbs@gmail.com>
2022-09-21 08:51:33 +0530

    End of Section 5

---
+++ NZWalks.API/NZWalks.API/Repositories/WalkRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public WalkRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<Walk> AddAsync(Models.Domain.Walk walk)
+        {
+            walk.Id = new Guid();
+            await nZWalksDbContext.Walks.AddAsync(walk);
+            await nZWalksDbContext.SaveChangesAsync();
+            return walk;
+
+        }
+
+        public async Task<Walk> DeleteAsync(Guid id)
+        {
+            var existingwalk = await nZWalksDbContext.Walks.FindAsync(id);
+            if (existingwalk == null) return null;
+
+            nZWalksDbContext.Remove(existingwalk);
+            await nZWalksDbContext.SaveChangesAsync();
+            return existingwalk;
+        }
+
+        public async Task<IEnumerable<Walk>> GetAllAsync()
+        {
+            return await
+                nZWalksDbContext.Walks
+                .Include(x=>x.Region)
+                .Include(x=>x.WalkDifficulty)
+                .ToListAsync();
+        }
+
+        public async Task<Walk> GetAsync(Guid id)
+        {
+            return await nZWalksDbContext.Walks
+                .Include(x=>x.Region)
+                .Include(x=>x.WalkDifficulty)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Walk> UpdateAsync(Guid id, Walk walk)
+        {
+            var existingWalk = await nZWalksDbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingWalk == null) return null;
+            
+            existingWalk.Length = walk.Length;
+            existingWalk.Name = walk.Name;            
+            existingWalk.RegionId = walk.RegionId;
+            existingWalk.WalkDifficultyId = walk.WalkDifficultyId;
+            await nZWalksDbContext.SaveChangesAsync();
+            return existingWalk;
+        }
+    }
+}
